81b2be61d7079615686fd23b7cf8cc23775e4152
Fitz-Also <>
2022-03-12 17:34:51 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import logo from './assets/svelte.png'
+  import Icon_nav_verticle from './comps/Icon_nav_verticle.svelte'
+  import Login_firebase from './comps/Login_firebase.svelte'
+</script>
+
+<main>
+  <Login_firebase/>
+  <h1>Hello world!</h1>
+
+</main>
+
+<style>
+
+</style>
