0c0badb89bccc7e8f6ac69c9b48b9c583abfa350
Fitz Also <101379237+Nickoslicko@users.noreply.github.com>
2022-03-12 18:20:33 +0000

    Added Goggle maps api

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,11 @@
   import logo from './assets/svelte.png'
   import Icon_nav_verticle from './comps/Icon_nav_verticle.svelte'
   import Login_firebase from './comps/Login_firebase.svelte'
+  import Google_maps_api from './comps/Google_maps_api.svelte'
 </script>
 
 <main>
-  <Login_firebase/>
+  <Google_maps_api/>
   <h1>Hello world!</h1>
 
 </main>
